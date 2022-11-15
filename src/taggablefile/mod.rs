5499c955f55d3285a2f29e3f9460f92f44f0c9c2
Frieder Hannenheim <frieder12.fml@pm.me>
2022-11-15 15:08:14 +0100

    TaggableFile work

--- src/taggablefile/mod.rs
+++ src/taggablefile/mod.rs
@@ -16,21 +16,22 @@ mod imp {
 
     #[derive(Default)]
     pub struct TaggableFile {
-        pub filename: RefCell<String>,
+        pub filename: RefCell<Option<String>>,
         pub title: RefCell<Option<String>>,
-        pub artists: RefCell<Vec<String>>,
-        pub duration: Cell<Option<f64>>,
         pub album: RefCell<Option<String>>,
-        pub album_artists: RefCell<Vec<String>>,
-        pub year: Cell<Option<i32>>,
-        pub cover: RefCell<Vec<u8>>,
-        pub cover_mime_type: RefCell<Option<String>>,
         pub composer: RefCell<Option<String>>,
-        pub disc: Cell<Option<u16>>,
-        pub total_discs: Cell<Option<u16>>,
-        pub track: Cell<Option<u16>>,
-        pub total_tracks: Cell<Option<u16>>,
         pub genre: RefCell<Option<String>>,
+        pub artists: RefCell<Vec<String>>,
+        pub album_artists: RefCell<Vec<String>>,
+
+        pub duration: Cell<Option<f64>>,
+        pub year: Cell<Option<i32>>,
+        pub disc: Cell<Option<u32>>,
+        pub total_discs: Cell<Option<u32>>,
+        pub track: Cell<Option<u32>>,
+        pub total_tracks: Cell<Option<u32>>,
+
+        pub cover: RefCell<Option<Texture>>,
     }
 
     #[glib::object_subclass]
@@ -45,14 +46,18 @@ mod imp {
                 vec![
                     ParamSpecString::builder("filename").build(),
                     ParamSpecString::builder("title").build(),
-                    ParamSpecValueArray::builder("artists").build(),
                     ParamSpecString::builder("album").build(),
+                    ParamSpecString::builder("composer").build(),
+                    ParamSpecString::builder("genre").build(),
+                    ParamSpecValueArray::builder("artists").build(),
+                    ParamSpecValueArray::builder("album_artists").build(),
+                    ParamSpecFloat::builder("duration").build(),
                     ParamSpecInt::builder("year").build(),
                     ParamSpecUInt::builder("disc").build(),
                     ParamSpecUInt::builder("total_discs").build(),
                     ParamSpecUInt::builder("track").build(),
                     ParamSpecUInt::builder("total_tracks").build(),
-                    ParamSpecString::builder("genre").build(),
+                    ParamSpecObject::builder::<Texture>("cover").build(),
                 ]
             });
             PROPERTIES.as_ref()
@@ -61,33 +66,48 @@ mod imp {
         fn set_property(&self, _id: usize, value: &Value, pspec: &ParamSpec) {
             if let Ok(value) = value.get::<String>() {
                 match pspec.name() {
-                    "filename" => self.filename.replace(Some(string_value.unwrap())),
-                    "title" => self.title.replace(Some(string_value.unwrap())),
-                    "album" => self.album.replace(Some(string_value.unwrap())),
-                    "genre" => self.genre.replace(Some(string_value.unwrap())),
+                    "filename" => self.filename.replace(Some(value)),
+                    "title" => self.title.replace(Some(value)),
+                    "album" => self.album.replace(Some(value)),
+                    "composer" => self.composer.replace(Some(value)),
+                    "genre" => self.genre.replace(Some(value)),
+                    "cover_mime_type" => self.cover_mime_type.replace(Some(value)),
                     _ => unimplemented!(),
                 }
             }
             if let Ok(value) = value.get::<Vec<String>>() {
                 match pspec.name() {
-                    "artists" => self.artists.replace(string_vec_value.unwrap()),
+                    "artists" => self.artists.replace(value),
+                    "album_artists" => self.album_artists.replace(value),
                     _ => unimplemented!(),
                 }
             }
             if let Ok(value) = value.get::<u32>() {
                 match pspec.name() {
-                    "disc" => self.disc.replace(Some(uint_value.unwrap() as u16)),
-                    "total_discs" => self.total_discs.replace(Some(uint_value.unwrap() as u16)),
-                    "track" => self.track.replace(Some(uint_value.unwrap() as u16)),
-                    "total_tracks" => self.total_tracks.replace(Some(uint_value.unwrap() as u16)),
+                    "disc" => self.disc.replace(Some(value)),
+                    "total_discs" => self.total_discs.replace(Some(value)),
+                    "track" => self.track.replace(Some(value)),
+                    "total_tracks" => self.total_tracks.replace(Some(value)),
                     _ => unimplemented!(),
                 }
             }
             if let Ok(value) = value.get::<i32>() {
                 match pspec.name() {
-                    "year" => self.year.replace(Some(int_value.unwrap())),
+                    "year" => self.year.replace(Some(value)),
                     _ => unimplemented!(),
                 };
+            }
+            if let Ok(value) = value.get::<f64>() {
+                match pspec.name() {
+                    "duration" => self.duration.replace(Some(value)),
+                    _ => unimplemented!(),
+                };
+            }
+            if let Ok(value) = value.get::<Texture>() {
+                match pspec.name() {
+                    "cover" => self.cover.replace(Some(value)),
+                    _ => unimplemented!(),
+                }
             }
         }
 
@@ -95,14 +115,20 @@ mod imp {
             match pspec.name() {
                 "filename" => self.filename.borrow().to_value(),
                 "title" => self.title.borrow().to_value(),
-                "artists" => self.artists.borrow().to_value(),
                 "album" => self.album.borrow().to_value(),
+                "composer" => self.composer.borrow().to_value(),
+                "genre" => self.genre.borrow().to_value(),
+                "artists" => self.artists.borrow().to_value(),
+                "album_artists" => self.album_artists.borrow().to_value(),
+
+                "duration" => self.duration.get().to_value(),
                 "year" => self.year.get().to_value(),
-                "disc" => self.disc.get().map(|inner| inner as u32).to_value(),
+                "disc" => self.disc.get().map().to_value(),
                 "total_discs" => self.total_discs.get().to_value(),
                 "track" => self.track.get().to_value(),
                 "total_tracks" => self.total_tracks.get().to_value(),
-                "genre" => self.genre.borrow().to_value(),
+
+                "cover" => self.cover.borrow().to_value(),
                 _ => unimplemented!(),
             }
         }
@@ -121,8 +147,13 @@ impl TaggableFile {
     pub fn new(
         filename: &str,
         title: &str,
-        artists: Vec<String>,
         album: &str,
+        composer: &str,
+        genre: &str,
+
+        artists: Vec<String>,
+        album_artists: Vec<String>,
+
         year: Option<i32>,
         disc: Option<u16>,
         total_discs: Option<u16>,
